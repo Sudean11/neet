04f1588af57be55cfe06472bd2dd2256c4947d67
Sudin Manandhar <sudin@Sudins-MacBook-Air.local>
2023-11-14 06:51:38 -0600

    neet code

---
+++ DuplicateArray.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int sum = 0;
+        HashMap<Integer, Integer> a = new HashMap<>();
+        for(int i = 0; i< nums.length; i++){
+            
+            if(a.containsKey(nums[i])){
+                return true;
+            }
+            a.put(nums[i], i);
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        System.out.println( new Solution().containsDuplicate(new int[]{3,3}));
+    }
+}
