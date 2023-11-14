2382cdb7a5d02744d8010a3ad503ad6d9f1dab72
Sudin Manandhar <sudin@Sudins-MacBook-Air.local>
2023-11-14 17:06:16 -0600

    anagram

---
+++ Anagram.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+      public boolean isAnagram(String s, String t) {
+        String[] a = s.split("");
+        List<String> b = new ArrayList<>( Arrays.asList(t.split("")));
+
+        if(a.length != b.size()){
+            return false;
+        }
+
+        for(int i = 0; i<a.length; i++){
+            if(b.contains(a[i])){
+                b.remove(a[i]);
+            }else{
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
+    public static void main(String[] args){
+        System.out.println( new Solution().isAnagram("sudsin", "niduass"));
+    }
+}
