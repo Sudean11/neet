540f5fbb04f9032840e94de112843c44501ed304
Sudin <sudean.mdrr@gmail.com>
2023-11-15 08:04:42 -0600

    remove duplicates

---
+++ RemoveDuplicates.java
@@ -0,0 +1,26 @@
+public class RemoveDuplicates {
+    
+    public static void main(String[] args) {
+        new RemoveDuplicates().removeDuplicates(new int[]{1,1,2});
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int newNumber = 0;
+        int previousNum = -198979;
+        int[] newArr = new int[nums.length];
+        for(int i: nums){
+            if(previousNum != nums[i]){
+                newArr[newNumber] = nums[i];
+                newNumber++;
+                previousNum = nums[i];
+            }else{
+                
+            }
+        }
+        nums = newArr;
+
+        System.out.println(nums[2]);
+        System.out.println(newNumber);
+        return newNumber;
+    }
+}
